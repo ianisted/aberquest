791dfd2f493b2b01c3c96b177b472f93c2e2f3f3
Ian Isted <ian@deerdigital.com>
2013-10-01 14:16:11 +0100

    hello billy

--- php/npc.php
+++ php/npc.php
@@ -2,10 +2,6 @@
 require_once ("Human.php");
 class Npc extends Human {
     
-<<<<<<< HEAD
-}
-=======
 }
 
-echo "hello world";
->>>>>>> d2c02540775873c1a139cb582f71c33f5dcd1765
+echo "hello billy";
