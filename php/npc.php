d4c86c4aff6c5a367d968b4b7672b86a94412ff2
Billy Fagan <billy@deerdigital.com>
2013-10-01 14:18:32 +0100

    afsd

--- php/npc.php
+++ php/npc.php
@@ -1,11 +1,5 @@
 <?php
 require_once ("Human.php");
 class Npc extends Human {
-    
-<<<<<<< HEAD
-}
-=======
-}
 
-echo "hello there billy again";
->>>>>>> 0100116522e8c499939a079de8380cba716c59a6
+}
