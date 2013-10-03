47be50b32deed8671a1bbc74c399b90d20fb15e2
billythekid <billythekid@unknown-00-24-36-b7-90-94.home>
2013-10-03 20:50:58 +0100

    updates to human class
    working through adding getters

--- php/human.php
+++ php/human.php
@@ -5,8 +5,7 @@
  * Extend with male/female/etc.
  */
 
-abstract class Human {
-    
+abstract class Human  {
     //instance variables
     
     private $id;
@@ -18,7 +17,7 @@ abstract class Human {
     private $height;
     private $weight;
     private $sleep_status;
-    private $observer;
+    private $observer_status;
     private $left_arm;
     private $right_arm;
     private $left_leg;
@@ -42,19 +41,23 @@ abstract class Human {
     private $home_location;
     
     
-    // Accessors
-    public function get_gender()
-    {
-        return $this->gender;
-    }
+    // Accessors - specifically defining these rather than using magic methods.
     
-    private function set_gender($gender = '')
-    {
-        $this->gender = $gender;
-    }
-    
-    public function get_height()
-    {
-        return $this->height;
-    }
+    public function get_id() { return $this->id; }
+    public function get_title() { return $this->title; }
+    public function get_first_name() { return $this->first_name; }
+    public function get_last_name() { return $this->last_name; }
+    public function get_full_name($string = false) { return ($string) ? "{$this->first_name} {$this->last_name}" : array('first_name'=>$this->first_name , 'last_name' => $this->last_name ); }
+    public function get_nickname() { return $this->nickname; }
+    public function get_gender() { return $this->gender; }
+    public function get_height() { return $this->height; }
+    public function get_weight() { return $this->weight; }
+    public function get_sleep_status() { return $this->sleep_status; }
+    public function get_observer_status() { return $this->observer_status; }
+    public function get_arms() { return array('left_arm'=>$this->left_arm,'right_arm'=>$this->right_arm); }
+    public function get_legs() { return array('left_leg'=>$this->left_leg,'right_leg'=>$this->right_leg); }
+    public function get_eyes() { return array('left_eye'=>$this->left_eye,'right_eye'=>$this->right_eye); }
+    public function get_hands() { return array('left_hand'=>$this->left_hand,'right_hand'=>$this->right_hand); }
+    public function get_ears() { return array('left_ear'=>$this->left_ear,'right_ear'=>$this->right_ear); }
+    public function get_current_location() { return $this->current_location; }
 }
