b13405dab6b0d75a9fb2410965e173a9326110b1
Billy Fagan <billy@deerdigital.com>
2013-10-07 14:03:57 +0100

    quick and durty API draft

---
+++ api/index.php
@@ -0,0 +1,38 @@
+<?php
+    
+    
+    function do_error($message = "Do What Now?")
+    {
+        array(
+            'type'=>'error',
+            'response_string' => $message
+        );
+    } 
+    
+    
+    
+    $return_array = array();
+
+    
+    if (!empty($_GET['action']))
+    {
+        /* this is just a mockup, really we'll have different files here based on actions probably, each of which will have their own rules etc. */
+        $action = $_GET['action'];
+        $action = strtok($action, " ");
+        $parameters = '';
+        $token = $action;
+        while ($token !== false)
+        {
+            $token = strtok(" ");
+            $paramaters .= " {$token}";
+        }
+        $return_array['type'] = "success";
+        $return_array['response_string'] = "You just asked me to {$action} on '{$paramaters}'";
+        
+    }
+    else
+    {
+        $return_array = do_error();
+    }
+    
+    echo json_encode($return_array);
