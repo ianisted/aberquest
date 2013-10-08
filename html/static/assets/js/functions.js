afb0967dc839e5eb111e5a95bb1e59ddb32bca5b
Ian Isted <ian@deerdigital.com>
2013-10-08 20:14:12 +0100

    Basic command input added
    CLI now accepts basic commands, allowing user to reconnect to server,
    clear screen, echo text, and a little easter egg added.

--- html/static/assets/js/functions.js
+++ html/static/assets/js/functions.js
@@ -17,3 +17,74 @@ function logger(string,logID,status) {
 	};
 	$('#terminal').scrollTop(900000);
 }
+
+
+var connection = false;
+
+
+var Game = {
+	init:function() {
+		logger('Aberquest version: ' + gameVersion);
+		
+		Game.connect();
+		
+		Game.Cmd.input();
+	},
+	Cmd: {
+		input:function() {
+			$('.cmd').keyup(function(event) {
+				if ( event.which == 13 ) {
+					var input = $('.cmd').val();
+					
+					if (input.toLowerCase().indexOf("<script") >= 0) {
+						logger("Not cool dude");
+						$('.cmd').val('');
+						return false;
+					}
+					
+					var cmd = $('.cmd').val().split(' ')[ 0 ].toLowerCase();
+					var string = input.substr(input.indexOf(" ") + 1);
+					
+					
+					switch(cmd) {
+						case "connect":
+							Game.connect();
+							break;
+						case "h4x0r": case "hacktheplanet": case "hacker": case "l33t": case "1337":
+							$('html').toggleClass('hacker');
+							break;
+						case "time": case "clock": case "date":
+							logger('<span class="error"><strong>' + cmd + '</strong>: ' + $.now() + '</span>');
+							break;
+						case "print": case "echo":
+							logger('<strong class="muted">' + string + '</strong>');
+							break;
+						case "clear": case "cls":
+							$('#terminal ul').html('');
+							break;
+						default:
+							logger('<span class="error"><strong>' + cmd + '</strong>: command not found</span>');
+					}
+					
+					$('.cmd').val('');
+				}
+				
+				
+			});
+		},
+		
+		
+	},
+	connect:function() {
+			logger('Connecting to server&hellip;');
+		
+			// Connect to server ;)
+			
+			if (connection) {
+				logger('Connected. Welcome to Aberquest.');
+			} else {
+				logger('There was a problem connecting. Type <strong>connect</strong> to retry at any time.');
+			}
+			
+		}
+}
